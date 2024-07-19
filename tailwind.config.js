173d81ef40e2dcc2fcda993b69c121c6780e3b27
wendyhamel <whamel@it4web.nl>
2024-07-19 14:33:16 +0200

    Started on styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Barlow', 'sans-serif'],
+      'special': ['Barlow Condensed', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        red:'hsl(0, 100%, 68%)',
+        blue: {
+          '400': 'hsl(230, 29%, 20%)',
+          '300': 'hsl(230, 11%, 40%)',
+          '200': 'hsl(231, 7%, 65%)',
+          '100': 'hsl(207, 33%, 95%)',
+        },
+      }
+    },
   },
   plugins: [],
 }
